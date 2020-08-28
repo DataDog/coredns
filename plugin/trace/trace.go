757d8b7c061ad9374f9278db3444506eefbdd78d
David Bouchare <david.bouchare@datadoghq.com>
2020-08-28 12:23:44 +0200

    Add config option to set up trace log level

--- plugin/trace/trace.go
+++ plugin/trace/trace.go
@@ -40,6 +40,7 @@ type trace struct {
 	every           uint64
 	count           uint64
 	Once            sync.Once
+	loglevel        bool
 }
 
 func (t *trace) Tracer() ot.Tracer {
@@ -54,7 +55,7 @@ func (t *trace) OnStartup() error {
 		case "zipkin":
 			err = t.setupZipkin()
 		case "datadog":
-			tracer := opentracer.New(tracer.WithAgentAddr(t.Endpoint), tracer.WithServiceName(t.serviceName), tracer.WithDebugMode(true))
+			tracer := opentracer.New(tracer.WithAgentAddr(t.Endpoint), tracer.WithServiceName(t.serviceName), tracer.WithDebugMode(t.loglevel))
 			t.tracer = tracer
 		default:
 			err = fmt.Errorf("unknown endpoint type: %s", t.EndpointType)
