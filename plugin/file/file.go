4b9f8a35ffc7b2d89380fad59479909b6a56ef37
Sergey Matyukevich <s.matyukevich@gmail.com>
2021-02-16 13:34:49 -0700

    Add file plugin metric

--- plugin/file/file.go
+++ plugin/file/file.go
@@ -123,6 +123,7 @@ func Parse(f io.Reader, origin, fileName string, serial int64) (*Zone, error) {
 	zp.SetIncludeAllowed(true)
 	z := NewZone(origin, fileName)
 	seenSOA := false
+	entriesByName := map[string]int{}
 	for rr, ok := zp.Next(); ok; rr, ok = zp.Next() {
 		if err := zp.Err(); err != nil {
 			return nil, err
@@ -139,10 +140,13 @@ func Parse(f io.Reader, origin, fileName string, serial int64) (*Zone, error) {
 				}
 			}
 		}
-
+		entriesByName[rr.Header().Name]++
 		if err := z.Insert(rr); err != nil {
 			return nil, err
 		}
+	}
+	for name, entries := range entriesByName {
+		zoneFileEntriesByDomain.WithLabelValues("dns_name:" + name).Set(float64(entries))
 	}
 	if !seenSOA {
 		return nil, fmt.Errorf("file %q has no SOA record for origin %s", fileName, origin)
